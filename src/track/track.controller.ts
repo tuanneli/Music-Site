819660ab60b7557850ec162e3e20afdf870cced5
tuanneli <zhuravlev99pic@gmail.com>
2022-10-01 21:27:09 +0300

    first commit

---
+++ src/track/track.controller.ts
@@ -0,0 +1,22 @@
+import {Controller} from "@nestjs/common";
+
+
+@Controller('/track')
+export class TrackController {
+
+    create() {
+
+    }
+
+    getAll() {
+
+    }
+
+    getOne() {
+
+    }
+
+    delete() {
+
+    }
+}
